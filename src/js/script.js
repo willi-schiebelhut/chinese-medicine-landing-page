54e8fba158e9224772c88592f17c1c271f6fd691
Willie <shibelgut.dev@gmail.com>
2020-06-26 21:09:34 +0600

    Add slider

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
           .addClass('therapy__tab_active').siblings().removeClass('therapy__tab_active')
           .closest('div.container').find('div.therapy__content').removeClass('therapy__content_active').eq($(this).index()).addClass('therapy__content_active');
       });
+
+    $('.doctors__inner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 4000,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/section_4/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/section_4/right.png"></button>'
+    });    
 });
+
