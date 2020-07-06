091a1984035946fefcca75b1244785a7b877de8c
Willie <shibelgut.dev@gmail.com>
2020-07-06 21:12:24 +0600

    mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        });
+    });
+});
+
 $(document).ready(function(){
     $('ul.therapy__tabs').on('click', 'li:not(.therapy__tab_active)', function() {
         $(this)
@@ -22,6 +40,58 @@ $(document).ready(function(){
         adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/section_4/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/section_4/right.png"></button>'
-    });    
+    });
+
+    //Modal
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation'). fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation'). fadeOut('fast');
+    });
+    $('.button_mini').on('click', function(){
+        $('.overlay, #consultation'). fadeIn('slow');
+    });
+    
+    $(document).mouseup(function (e){
+        var modalctr = $(".overlay");
+        var modal = $("#consultation");
+        if (!modal.is(e.target) && modal.has(e.target).length === 0){
+        modalctr.hide();
+        }
+    });
+    $('.modal-form').validate({
+        rules:{
+            name: {
+                required: true,
+                minlength: 2
+              },
+            phone: "required"
+        },
+        messages: {
+            name:{
+                required: "Пожалуйста, введите своё имя",
+                minlength: jQuery.validator.format("Введите как минимум {0} символа!")
+              },
+            phone: "Пожалуйста, введите номер телефона"
+          }
+        });
+        $('input[name=phone]').mask("+7 (999) 999-99-99");
+        //pageup
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 1600) {
+                $('.pageup').fadeIn();
+            } else {
+                $('.pageup').fadeOut();
+            }
+        });
+
+        $("a[href^='#']").click(function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+        });
 });
+
+
 
